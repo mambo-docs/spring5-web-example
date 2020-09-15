4bdca19b10786c3d23b2314937c49075ada13e25
kdevkr <kdevkr@gmail.com>
2020-09-15 15:03:07 +0900

    initialize springframework.

---
+++ src/test/java/com/example/demo/SpringApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.junit.Test;
+
+public class SpringApplicationTests {
+
+    @Test
+    public void test() throws Exception {
+        SpringApplication.main(new String[]{});
+    }
+}
