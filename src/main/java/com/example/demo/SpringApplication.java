4831e016a60c057ac31f24b228087e51da31932a
kdevkr <kdevkr@gmail.com>
2020-09-15 15:23:46 +0900

    Apply i18n using MessageSource
    (cherry picked from commit ea527e8507da7ffb814c2a507d1583e59e60fb0e)

--- src/main/java/com/example/demo/SpringApplication.java
+++ src/main/java/com/example/demo/SpringApplication.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import com.example.demo.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.util.Locale;
 
 public class SpringApplication {
 
@@ -9,5 +12,12 @@ public class SpringApplication {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         boolean active = applicationContext.isActive();
         System.out.println("applicationContext is active " + active);
+
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        String applicationName = environment.getProperty("spring.application.name", String.class);
+        System.out.println("spring.application.name : " + applicationName);
+
+        String message = applicationContext.getMessage("argument.required", new Object[]{"message"}, Locale.getDefault());
+        System.out.println(message);
     }
 }
