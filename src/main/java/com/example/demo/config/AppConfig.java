ea527e8507da7ffb814c2a507d1583e59e60fb0e
kdevkr <kdevkr@gmail.com>
2020-09-15 15:23:46 +0900

    Apply i18n using MessageSource

--- src/main/java/com/example/demo/config/AppConfig.java
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -1,15 +1,25 @@
 package com.example.demo.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 /**
  * Root Application Configuration Metadata
  *
  * @author mambo
  */
+@ComponentScan(basePackages = {"com.example.demo"})
 @PropertySource({"classpath:application.properties"})
 @Configuration
 public class AppConfig {
-
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("i18n/messages");
+        return messageSource;
+    }
 }
