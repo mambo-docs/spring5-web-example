de66b9a64766288b0719e3451e1568d1409dd122
kdevkr <kdevkr@gmail.com>
2020-09-17 18:35:51 +0900

    Session Management
    (cherry picked from commit dd07a701d74619e5c638eedf025a9fa7a4e09afe)

---
+++ src/main/java/com/example/demo/config/HttpSessionApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.example.demo.config;
+
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+public class HttpSessionApplicationInitializer extends AbstractHttpSessionApplicationInitializer {
+}
