8d9c57315af053af5b30822ff10a5deba4378e05
kdevkr <kdevkr@gmail.com>
2020-09-24 23:10:59 +0900

    Security

---
+++ src/main/java/com/example/demo/config/WebSecurityApplicationInitializer.java
@@ -0,0 +1,5 @@
+package com.example.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class WebSecurityApplicationInitializer extends AbstractSecurityWebApplicationInitializer {}
